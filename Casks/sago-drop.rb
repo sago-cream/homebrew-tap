580acf86af648dc9bcbb44ea8f9ce6095e2de1ba
Hsi <its.hsichen@gmail.com>
2026-08-30 22:01:52 +0800

    chore: update sago-drop to v0.9.0

--- Casks/sago-drop.rb
+++ Casks/sago-drop.rb
@@ -1,12 +1,13 @@
 cask "sago-drop" do
-  version "0.8.0"
-  sha256 "244cfed272db49ff0c11257e2d02360c6625dcea8ea200bd5c594f9b0def1928"
+  version "0.9.0"
+  sha256 "0601cceadc0f07e4ca65c969f7ca8b5c51c362fe9e8cd3c7dc2e271754f091b4"
 
   url "https://github.com/sago-cream/sago-drop/releases/download/v#{version}/Sago-Drop-#{version}.zip"
   name "Sago Drop"
   desc "Upload files from the menu bar"
   homepage "https://github.com/sago-cream/sago-drop"
 
+  auto_updates true
   depends_on macos: :sonoma
 
   app "Sago Drop.app"
