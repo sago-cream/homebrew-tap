24ed08bb0466b5f34947396551b992db7109fd16
Hsi <its.hsichen@gmail.com>
2026-09-02 15:56:53 +0800

    chore: update sago-drop to v0.10.1

--- Casks/sago-drop.rb
+++ Casks/sago-drop.rb
@@ -1,6 +1,6 @@
 cask "sago-drop" do
-  version "0.10.0"
-  sha256 "94eca1f8c0aaee283b872af0b7b1df43ff43bc249479d645736a83a0872ff4e4"
+  version "0.10.1"
+  sha256 "f953cd701cf8946ab9a51076510138e032c081710630f968925bca4fbc4c402f"
 
   url "https://github.com/sago-cream/sago-drop/releases/download/v#{version}/Sago-Drop-#{version}.zip"
   name "Sago Drop"
